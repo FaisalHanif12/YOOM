1d14684d86451390f4a2a7da985aeec7479fcc1f
Faisal Hanif <mehrfaisal111@example.com>
2024-09-20 16:19:44 -0700

    Update layout

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -6,7 +6,7 @@ import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
   title: 'YOOM',
-  description: 'A workspace for your team, powered by Stream Chat and Clerk.',
+  descriptions: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
 const RootLayout = ({ children }: Readonly<{children: ReactNode}>) => {
